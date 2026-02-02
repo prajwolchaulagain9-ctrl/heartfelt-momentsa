16f3374d26b8bf6edb03133d5a1ef459bc3ba1c3
Prajwol Chaulagain <prajwolchaulagain9@gmail.com>
2026-02-02 14:48:11 +0000

    wow what a freaking project

--- src/components/PhotoShowcase.tsx
+++ src/components/PhotoShowcase.tsx
@@ -10,23 +10,23 @@ interface PhotoShowcaseProps {
 const photos = [
   {
     id: 1,
-    url: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&h=500&fit=crop",
-    praise: "Your kindness lights every room",
+    url: "https://scontent.fktm8-1.fna.fbcdn.net/v/t1.15752-9/622647849_2499634570432038_8263838319862653837_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=RaVe4h2P3x4Q7kNvwGzzR6N&_nc_oc=Adki1M6lmmB1rKVMYXyD9G0slsJlresdOj9_wgQ-84nyWb51I2mKh6m6j6kcOyyhiTzKsjp4xuTYAvvVZZW_48_Q&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&oh=03_Q7cD4gHOGMOMoCa_lyT6ZbX47vw7fNi06BgJJqRvu6e5klIjbQ&oe=69A80CDF",
+    praise: "Your beauty shines from within",
   },
   {
     id: 2,
-    url: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400&h=500&fit=crop",
-    praise: "That smile softens every day",
+    url: "https://scontent.fktm8-1.fna.fbcdn.net/v/t1.15752-9/619251981_785952424515798_7570970355521022386_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=esnsn0IfFo8Q7kNvwG4wVBS&_nc_oc=AdkA_M_unTgIU1LkpPoHD9huyC7xlwGB_XzhnMjTqiRhiToCRpv-AYnuJ0MAUTKRId5jphp4XMAhgDe5QKb_IupJ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&oh=03_Q7cD4gFt_JkMVod6JGLfXWLJGyhshL5tkIfYoSmbp97nxJTwag&oe=69A8285E",
+    praise: "That smile takes my breath away",
   },
   {
     id: 3,
-    url: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=400&h=500&fit=crop",
-    praise: "You carry grace without trying",
+    url: "https://scontent.fktm8-1.fna.fbcdn.net/v/t1.15752-9/624278459_1227251148746712_1618744897501690900_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=XYbVIuLj5ycQ7kNvwH1fsJ8&_nc_oc=AdmEfGqsdvpifkd8DpuYRexolzoHqfKU1zSgtlHqDQ_tmVSGebDAin0heyvJd3oI8FY9Z7IWY87Hol6x_kyIk-E1&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&oh=03_Q7cD4gF3vIr5RxWNoBken-EyP6EpmqcyWi25z9siUdEQ5xJWmQ&oe=69A82100",
+    praise: "Your grace mesmerizes me",
   },
   {
     id: 4,
-    url: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&h=500&fit=crop",
-    praise: "Every ordinary moment feels golden with you",
+    url: "https://scontent.fktm8-1.fna.fbcdn.net/v/t1.15752-9/620637700_1572543807533655_6255011560553014012_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=48oRdPFoTWMQ7kNvwG97bJa&_nc_oc=Admjxze_j5eSwSApiMSftIOLfcd5cVjNhFfKOlYMAAiA43CHs-R1NuMLFdvOfPXbV3fB4CNKtwjLAZ3NC-Cuzuy-&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&oh=03_Q7cD4gHe4Q5-EURDCLTiRh0P2pu3r5UUKYXeOFSPPQ05r5p50Q&oe=69A82389",
+    praise: "Every moment with you is pure magic",
   },
 ];
 
